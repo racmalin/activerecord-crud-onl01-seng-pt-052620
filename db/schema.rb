14d4c296f3e2fe24202b19cfd86f048ec773ac7f
Kevin C Malin <racmalin@yahoo.com>
2020-08-04 03:43:35 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,10 @@
 ActiveRecord::Schema.define(version: 2020_08_04_032541) do
+ create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
 
 end
